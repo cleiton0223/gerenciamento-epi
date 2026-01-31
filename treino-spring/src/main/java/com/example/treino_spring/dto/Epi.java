84e08f12035f49b902af7ecd2f4d1eadcdacdc29
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2026-01-30 21:48:59 -0300

    feat:adicionar dto de epi

---
+++ treino-spring/src/main/java/com/example/treino_spring/dto/Epi.java
@@ -0,0 +1,46 @@
+package com.example.treino_spring.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Epi {
+
+    private Long idEpi;
+
+    @NotBlank(message = "Nome não pode ser nulo")
+    @Column(nullable = false,unique = true)
+    @Size(max = 120,message ="Nome não pode ultrapassar os 120 caracteres")
+    private String nome;
+
+    @NotBlank
+    @Column(nullable = false)
+    @Size(max = 80,message = "O tipo de epi não pode ultrapassar os 80 caracteres")
+    private String tipoEpi;
+
+    @NotNull
+    @Size(max = 255,message = "A descrição não pode ultrapassar os 255 caracteres")
+    private String descricao;
+
+    @NotBlank
+    @Column(nullable = false)
+    @Size(max = 20,message = "Tamanho não pode ultrapassar os 20 caracteres")
+    private String tamanho;
+
+    private LocalDate validade;
+
+    @NotBlank
+    @Column(nullable = false)
+    @Size(max = 20,message = "Situação não pode ultrapssar os 20 caracteres")
+    private String situacao;
+
+}
