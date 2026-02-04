bd057ed18ccb94d6b2458c52f1033a52979ed0b5
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2026-02-04 20:51:17 -0300

    fix: corrigir o tipo da entidade no autor epi repository

--- treino-spring/src/main/java/com/example/treino_spring/Repository/EpiRepository.java
+++ treino-spring/src/main/java/com/example/treino_spring/Repository/EpiRepository.java
@@ -1,8 +1,9 @@
 package com.example.treino_spring.Repository;
 
 
+import com.example.treino_spring.entity.Epi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface EpiRepository extends JpaRepository<EpiRepository,Long> {
+public interface EpiRepository extends JpaRepository<Epi,Long> {
 }
